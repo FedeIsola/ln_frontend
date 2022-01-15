99ac95b7f028286bded5e93bf1cab0b9e2e1fab9
Federico Isola <fedeisola2007@gmail.com>
2022-01-15 17:08:16 -0300

    Added dummy data

---
+++ src/api/news.js
@@ -0,0 +1,153 @@
+export const news = [
+  {
+    lead: "lead1",
+    titulo: "titulo1",
+    bajada: "bajada1",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee1",
+    nombreAutor: "autor1",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead2",
+    titulo: "titulo2",
+    bajada: "bajada2",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee2",
+    nombreAutor: "autor2",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead3",
+    titulo: "titulo3",
+    bajada: "bajada3",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee3",
+    nombreAutor: "autor3",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead4",
+    titulo: "titulo4",
+    bajada: "bajada4",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee4",
+    nombreAutor: "autor4",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead5",
+    titulo: "titulo5",
+    bajada: "bajada5",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee5",
+    nombreAutor: "autor5",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead6",
+    titulo: "titulo6",
+    bajada: "bajada6",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee6",
+    nombreAutor: "autor6",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead7",
+    titulo: "titulo7",
+    bajada: "bajada7",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee7",
+    nombreAutor: "autor7",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead8",
+    titulo: "titulo8",
+    bajada: "bajada8",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee8",
+    nombreAutor: "autor8",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead9",
+    titulo: "titulo9",
+    bajada: "bajada9",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee9",
+    nombreAutor: "autor9",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead10",
+    titulo: "titulo10",
+    bajada: "bajada10",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee10",
+    nombreAutor: "autor10",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead11",
+    titulo: "titulo11",
+    bajada: "bajada11",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee11",
+    nombreAutor: "autor11",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead12",
+    titulo: "titulo12",
+    bajada: "bajada12",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee12",
+    nombreAutor: "autor12",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead13",
+    titulo: "titulo13",
+    bajada: "bajada13",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee13",
+    nombreAutor: "autor13",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead14",
+    titulo: "titulo14",
+    bajada: "",
+    imagen: "https://picsum.photos/500",
+    marquee: "",
+    nombreAutor: "autor14",
+    imagenAutor: "https://picsum.photos/64",
+    url: "https://lanacion.com.ar"
+  },
+  {
+    lead: "lead15",
+    titulo: "titulo15",
+    bajada: "bajada15",
+    imagen: "https://picsum.photos/500",
+    marquee: "marquee15",
+    nombreAutor: "",
+    imagenAutor: "",
+    url: "https://lanacion.com.ar"
+  },
+
+]
