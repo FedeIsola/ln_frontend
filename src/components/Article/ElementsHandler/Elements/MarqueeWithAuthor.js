b960324eb54a6c658b94a6eac7ecb013ddc026c5
Federico Isola <fedeisola2007@gmail.com>
2022-01-19 02:55:01 -0300

    Fixed render condition in MarqueeWithAuthor

--- src/components/Article/ElementsHandler/Elements/MarqueeWithAuthor.js
+++ src/components/Article/ElementsHandler/Elements/MarqueeWithAuthor.js
@@ -1,12 +1,13 @@
 const MarqueeSpan = (props) => <span className="marquee bold-text">{props.marquee}</span>;
 const AuthorNameSpan = (props) => <span className="author-name bold-text">Por {props.authorName}</span>;
 
-const MarqueeWithAuthor = (props) => (
-  <p className="marquee-with-author">
-    {props.marquee ? <MarqueeSpan marquee={props.marquee} /> : null}
-    {props.marquee && props.authorName ? <span> / </span> : null}
-    {props.authorName ? <AuthorNameSpan authorName={props.authorName} /> : null}
-  </p>
-);
+const MarqueeWithAuthor = (props) =>
+  props.marquee || props.author ? (
+    <p className="marquee-with-author">
+      {props.marquee ? <MarqueeSpan marquee={props.marquee} /> : null}
+      {props.marquee && props.authorName ? <span> / </span> : null}
+      {props.authorName ? <AuthorNameSpan authorName={props.authorName} /> : null}
+    </p>
+  ) : null;
 
 export default MarqueeWithAuthor;
