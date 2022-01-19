8817b82df7fc451a0152298226302d0a835a1ee2
Federico Isola <fedeisola2007@gmail.com>
2022-01-19 00:11:23 -0300

    Implemented Containers in App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import "./App.scss";
-import { news } from "./api/news";
-import { Article } from "./components";
+import { articles } from "./api/articles";
+import { Container } from "./components";
 
 const App = () => {
   return (
-    <main>
-      <div className="notasx3">
-        {news.map(el => {
-          const random = Math.floor(Math.random() * 3) + 1;
-          switch (random) {
-            case 1:
-              return <Article data={el} />
-            case 2:
-              return <Article data={el} type="author-scoped" />
-            case 3:
-              return <Article data={el} type="color-scoped" />
-            default: <Article data={el} />;
-          }
-        })}
-      </div>
+    <main className="main-content">
+      {articles.map((item, index) => {
+        return <Container key={index} title={item.title} articles={item.collection} type={item.type} />;
+      })}
     </main>
   );
-}
+};
 
 export default App;
