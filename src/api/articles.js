a5ffa7fad246fbb84d1c0cc6fce033ea2e418185
Federico Isola <fedeisola2007@gmail.com>
2022-01-19 00:10:38 -0300

    Changed news file to articles, added collections

---
+++ src/api/articles.js
@@ -0,0 +1,148 @@
+export const articles = [
+  {
+    title: "Novedades",
+    type: "focal-izquierdo",
+    url: "https://www.lanacion.com.ar/novedades",
+    collection: [
+      {
+        lead: "Meter la pata",
+        title: "Tras verse con Alberto Fernandez, Axel Kicillof volvió a criticar al FMI",
+        tail: "Mientras el Gobierno intenta destrabar las negociaciones, el gobernador arremetió contra el organismo internacional.",
+        image:
+          "https://resizer.glanacion.com/resizer/J1whVIU23soMWXO9r1deW1zsM_0=/560x373/smart/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/RLENYAUD4VBQ3ARDX66PONOMJY.jpg",
+        authorName: "Pedro Lacour",
+        url: "https://www.lanacion.com.ar/politica/tras-verse-con-alberto-fernandez-axel-kicillof-volvio-a-criticar-al-fmi-nid17012022/",
+      },
+      {
+        lead: "Grave denuncia",
+        title: "Un docente afectado por el PreViaje fue increpado por Lammens tras hablar con LA NACION",
+        image:
+          "https://resizer.glanacion.com/resizer/s3DgvjuTIWPTMwoyHdUYcpJxnN8=/292x194/smart/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/VT4EBBTXFJH7PBJZ77B5ZISTOY.JPG",
+        authorName: "Bárbara Epsztein",
+        url: "https://www.lanacion.com.ar/politica/uno-de-los-afectados-por-el-previaje-denuncio-que-el-ministro-matias-lammens-lo-increpo-por-chat-vos-nid17012022/",
+      },
+      {
+        lead: "Fiscal apartado",
+        title: "Es militante antivacunas e investigaba la muerte de una mujer inmunizada con AstraZeneca",
+        image:
+          "https://resizer.glanacion.com/resizer/QfYAU0OCHrPhMJqSgWSQUtFqp_w=/292x194/smart/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/7FHTSWMUEVGP7DZPQ54PVQ64CI.JPG",
+        authorName: "Gabriel Di Nicola",
+        url: "https://www.lanacion.com.ar/sociedad/fiscal-apartado-es-militante-antivacunas-e-investigaba-la-muerte-de-una-mujer-inmunizada-con-nid17012022/",
+      },
+    ],
+  },
+  {
+    type: "notas-3",
+    collection: [
+      {
+        lead: "Coronavirus",
+        title: "Cuáles son las condiciones que pide España para el ingreso de los argentinos al país",
+        image:
+          "https://resizer.glanacion.com/resizer/WDVpfTrXjZnl-8hvnlE_7Qpac60=/298x200/smart/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/AS2AR36AOFHT5FF7GHDDJUM2EU",
+        url: "https://www.lanacion.com.ar/el-mundo/cuales-son-los-nuevos-requisitos-que-pide-espana-para-el-ingreso-de-los-argentinos-al-pais-nid17012022/",
+      },
+      {
+        lead: "Punta Mogotes",
+        title: "Vuelve el hijo del Pato de Mar del Plata, el ícono detrás de un negocio de 52 años",
+        image:
+          "https://resizer.glanacion.com/resizer/MRI6JgY0FOmcw9ZC7ee9l5VYXqo=/298x200/smart/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/QWZFY47RP5DHRP3NBOAFKG6IJA.JPG",
+        authorName: "Angie Anglesio",
+        url: "https://www.lanacion.com.ar/economia/punta-mogotes-vuelve-el-hijo-del-pato-de-mar-del-plata-el-icono-detras-de-un-negocio-de-52-anos-nid17012022/",
+      },
+      {
+        lead: "Desde $1800",
+        title: "Cómo invertir, a partir de mañana, en los principales índices de Estados Unidos desde la Argentina",
+        image:
+          "https://resizer.glanacion.com/resizer/d5J-K3979ZgTC0DOhAvYDoFlFAg=/298x200/smart/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/XZZOE4WD2RBGRPIN7PQ55222TQ.jpeg",
+        authorName: "Melisa Reinhold",
+        url: "https://www.lanacion.com.ar/economia/nueva-normativa-como-invertir-desde-la-argentina-por-menos-de-2000-en-las-principales-empresas-de-nid17012022/",
+      },
+    ],
+  },
+  {
+    type: "notas-3",
+    collection: [
+      {
+        title: "Facturas baratitas, pero todos al spiedo",
+        authorName: "Pablo Sirvén",
+        authorImage:
+          "https://resizer.glanacion.com/resizer/Os-TLy48VlJJ7ptRvoq3I7lbcRg=/80x0/filters:quality(80)/s3.amazonaws.com/arc-authors/lanacionar/2735400.png",
+        url: "https://www.lanacion.com.ar/opinion/facturas-baratitas-pero-todos-al-spiedo-nid16012022/",
+        type: "author-scoped",
+      },
+      {
+        lead: "En fotos",
+        title: "Esta casa no tiene Internet, se arma en 90 minutos y se conecta con el celular",
+        image:
+          "https://resizer.glanacion.com/resizer/AQvLqGYLhPERoU6BIRUNJWXD2oc=/320x213/smart/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/AVSH3ZEVOBAM3FSQW6QG4NFJ5M.jpg",
+        authorName: "Lucila Barreiro",
+        url: "https://www.lanacion.com.ar/propiedades/construccion-y-diseno/esta-casa-no-tiene-internet-se-arma-en-90-minutos-y-se-conecta-con-el-celular-nid13012022/",
+      },
+      {
+        lead: "Con un avión militar",
+        title: "La fuerza Aérea operará vuelos de Flybondi ante falta de pilotos por el avance de Ómicron",
+        image:
+          "https://resizer.glanacion.com/resizer/ee8HkIWerblA5-qkF9KUQ8lj_DQ=/320x213/smart/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/ZNWLCSZIHVFAHAYNTUPHKR55FI.JPG",
+        authorName: "Mariano De Vedia",
+        url: "https://www.lanacion.com.ar/politica/la-fuerza-aerea-operara-vuelos-de-flybondi-por-falta-de-pilotos-ante-el-avance-de-casos-de-covid-19-nid17012022/",
+      },
+    ],
+  },
+  {
+    type: "notas-3",
+    collection: [
+      {
+        lead: "Ni la costa ni el Sur",
+        title: "El otro destino nacional que, pese a la ola de contagios, vive un boom turístico",
+        image:
+          "https://resizer.glanacion.com/resizer/fd9_2Phbcf80QoV0BquGyFJYbd8=/233x159/smart/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/U3KTAINKBFDEFKHTBL6HETMB6Q.webp",
+        authorName: "Gabriela Origlia",
+        url: "https://www.lanacion.com.ar/sociedad/ni-la-costa-ni-el-sur-el-otro-destino-nacional-que-pese-a-la-ola-de-contagios-vive-un-boom-turistico-nid17012022/",
+      },
+      {
+        lead: "Viral imperdible",
+        title: "El video que muestra que toda la vida usamos mal las almohadas para viajar",
+        image:
+          "https://resizer.glanacion.com/resizer/tPg0kCW4Icp7F5vWVSAGisfj5wI=/233x159/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/DBU4AXZROJH65JX6P2I7VSPD4Y.jpg",
+        url: "https://www.lanacion.com.ar/lifestyle/el-video-que-muestra-que-toda-la-vida-usamos-mal-las-almohadas-para-viajar-nid17012022/",
+        marquee: "Esto es un marquee de test",
+      },
+      {
+        lead: "Alguien denunció",
+        title: "La bronca de Ivana Nadal por no poder quedarse a vivir en Estados Unidos: los motivos",
+        image:
+          "https://resizer.glanacion.com/resizer/rwP-Am8PKS8OHmdkhEOTm9HEafk=/233x159/smart/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/56Z3HJHMTFD2XAUIXHEF4GA75M.jpg",
+        url: "https://www.lanacion.com.ar/espectaculos/la-bronca-de-ivana-nadal-por-no-poder-quedarse-a-vivir-en-los-estados-unidos-alguien-denuncio-nid16012022/",
+      },
+    ],
+  },
+  {
+    type: "notas-3",
+    collection: [
+      {
+        title: "Bien común e interés publico, fines del Estado",
+        authorName: "Juan Carlos Cassagne",
+        authorImage:
+          "https://resizer.glanacion.com/resizer/x4iwhDNPRHX2l88RARa9_GSgU6A=/80x0/filters:quality(80)/s3.amazonaws.com/arc-authors/lanacionar/2865294.png",
+        url: "https://www.lanacion.com.ar/opinion/bien-comun-e-interes-publico-fines-del-estado-nid17012022/",
+        type: "author-scoped",
+      },
+      {
+        lead: "Super alta definición",
+        title: "Netflix en calidad 4K: cómo acceder a una imagen de cine en tu casa",
+        image:
+          "https://resizer.glanacion.com/resizer/poErdqh0dAdirjVsohKnxnXUC98=/233x159/smart/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/GDM57HS7I5D3HMXEDNHAT53DT4.jpg",
+        url: "https://www.lanacion.com.ar/lifestyle/netflix-en-calidad-4k-uhd-como-acceder-a-una-imagen-como-del-cine-pero-en-tu-casa-nid17012022/",
+      },
+      {
+        lead: "Previsión",
+        title: "La F.1 tomó nota del caso Djokovic e impuso su propia regla para evitar sorpresas",
+        marquee: "Test",
+        image:
+          "https://resizer.glanacion.com/resizer/0xKwLVupHrlnDspDwvuksEh-OPs=/231x154/smart/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/HMH4V7PGCVEZTFW4FIOWCZEP3U.JPG",
+        url: "https://www.lanacion.com.ar/deportes/la-formula-1-obligara-a-todos-sus-pilotos-a-estar-vacunados-esta-temporada-nid17012022/",
+        type: "color-scoped",
+      },
+    ],
+  },
+];
