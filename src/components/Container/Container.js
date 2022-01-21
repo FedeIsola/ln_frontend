38d3227e95c4eb00a183b9d49ac211afdbb80d8f
Federico Isola <fedeisola2007@gmail.com>
2022-01-21 12:08:09 -0300

    Add url redirection in container title

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,10 +1,10 @@
 import "./Container.scss";
 import { Layout } from "./LayoutsHandler/Layout";
 
-export const Container = ({ type, articles, title }) => {
+export const Container = ({ type, articles, title, url }) => {
   return (
     <section className="container">
-      {title ? <h2 className="container-title">{title}</h2> : null}
+      {title ? <h2 className="container-title"> {url ? <a className="container-link" href={url}>{`${title}`}</a> : `${title}`}</h2> : null}
       <section className={`inner-container ${type ? type : null}`}>
         <Layout articles={articles} type={type} />
       </section>
