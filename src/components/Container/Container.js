8fb69a20b85dcb1bc53752064d1e1b9f3066b5d8
Federico Isola <fedeisola2007@gmail.com>
2022-01-20 23:36:00 -0300

    Add title in container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -5,8 +5,11 @@ export const Container = (props) => {
   const { type, articles, title } = props;
 
   return (
-    <section className={`container ${type ? type : null}`}>
-      <Layout articles={articles} type={type} title={title} />
+    <section className="container">
+      {title ? <h2 className="container-title">{title}</h2> : null}
+      <section className={`inner-container ${type ? type : null}`}>
+        <Layout articles={articles} type={type} />
+      </section>
     </section>
   );
 };
